081c5ecefa5a7e7fcb9f5b9368f916b58eddd664
Kyle Garner <garnerk10@gmail.com>
2023-01-04 14:10:13 -0500

    started etch-a-sketch project

---
+++ eas.js
@@ -0,0 +1,70 @@
+let gridSize = 16;
+let mode = `black`
+
+const grid = document.getElementById('grid');
+const clearButton = document.getElementById('clear-button');
+const colorButton = document.getElementById('color-button');
+const blackButton = document.getElementById('black-button');
+const rainbowButton = document.getElementById('rainbow-button');
+const sizeButton = document.getElementById('size-button');
+
+//Generate the grid
+const generateGrid = (size) => {
+    grid.innerHTML = "";
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < size * size; i++){
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.style.backgroundColor = 'white';
+        gridElement.addEventListener('mouseover', colorChange);
+        grid.appendChild(gridElement);
+    };
+
+};
+
+//Color change function for each div in grid
+const colorChange = (event) => {
+    if(mode === 'black'){event.target.style.backgroundColor = `rgb(0, 0, 0)`}
+    else if (mode === 'rainbow'){
+        let colorR = Math.floor(Math.random() * 256);
+        let colorG = Math.floor(Math.random() * 256);
+        let colorB = Math.floor(Math.random() * 256);
+
+        event.target.style.backgroundColor = `rgb(${colorR}, ${colorG}, ${colorB})`
+    };
+};
+
+//Change mode to black
+const blackMode = () => {
+    mode = 'black'
+};
+blackButton.addEventListener('click', blackMode);
+
+//Change mode to rainbow
+const rainbowMode = () => {
+    mode = 'rainbow'
+};
+rainbowButton.addEventListener('click', rainbowMode);
+
+//clear button and function
+const clear = () => {
+    grid.innerHTML = "";
+    generateGrid(gridSize);
+};
+clearButton.addEventListener('click', clear);
+
+//grid size change and function
+const changeSize = () => {
+    let newDimension = prompt("Enter a new dimension");
+    if(newDimension !== null && newDimension <= 100 && newDimension > 0){
+        gridSize = newDimension;
+        generateGrid(gridSize);
+    } else {alert(`Please enter a dimension between 1 and 100.`)};
+};
+sizeButton.addEventListener('click', changeSize);
+
+window.onload = () => {
+    generateGrid(gridSize);
+}
